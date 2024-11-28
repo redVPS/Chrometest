9ce5a83c35b69bfc6ebec2cbb958d6daa239722c
redVPS <157881932+redVPS@users.noreply.github.com>
2024-11-28 21:43:23 +0530

    Update AppTest.java

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -1,14 +1,17 @@
 import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
 
 public class AppTest {
-    
+
     @Test
     public void testIsChromeInstalled() {
         // Assuming Chrome is installed at the default location
         String chromePath = "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe";
-        assertTrue("Chrome should be installed", App.isChromeInstalled(chromePath));
+        
+        // Test if Chrome is installed at the given path
+        boolean isInstalled = App.isChromeInstalled(chromePath);
+        
+        // Assert that Chrome should be installed at the expected path
+        assertTrue("Chrome should be installed", isInstalled);
     }
 }
-
